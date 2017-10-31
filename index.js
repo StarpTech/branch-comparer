e11fec98490a7c784608effc4b91817b967daee4
Dustin Deus <deusdustin@gmail.com>
2017-11-01 00:09:20 +0100

    use async

--- index.js
+++ index.js
@@ -8,52 +8,49 @@ const RandomColor = require('randomcolor')
 const SpawnSync = require('child_process').spawnSync
 const git = require('simple-git/promise')(process.cwd())
 
-git
-  .branch()
-  .then(result => {
-    const branches = result.all
+async function init() {
+  const result = await git.branch()
 
-    return Inquirer.prompt([
-      {
-        type: 'checkbox',
-        name: 'choice',
-        message: "What's your branches to compare?",
-        choices: branches,
-        validate: input => {
-          if (input.length < 2) {
-            return 'You have to select two branches!'
-          }
-          return true
+  const branches = result.all
+
+  const branchPrompt = await Inquirer.prompt([
+    {
+      type: 'checkbox',
+      name: 'choice',
+      message: "What's your branches to compare?",
+      choices: branches,
+      validate: input => {
+        if (input.length < 2) {
+          return 'You have to select two branches!'
         }
+        return true
       }
-    ]).then(result => {
-      const choices = result.choice
+    }
+  ])
 
-      return Inquirer.prompt([
-        {
-          type: 'input',
-          name: 'cmd',
-          message:
-            'Please enter the command you want to execute in both branches!'
-        }
-      ]).then(result => {
-        const cmd = result.cmd
+  const choices = branchPrompt.choice
 
-        choices.forEach(async branch => {
-          console.log(Chalk.hex(RandomColor())(`Checking out "${branch}"`))
-          await git
-            .checkout(branch)
-            .then(() => {
-              console.log(Chalk.grey(`Execute "${cmd}"`))
-              SpawnSync(cmd, {
-                stdio: 'inherit',
-                shell: true,
-                encoding: 'utf-8'
-              })
-            })
-            .catch(console.error)
-        })
-      })
+  const cmdPrompt = await Inquirer.prompt([
+    {
+      type: 'input',
+      name: 'cmd',
+      message: 'Please enter the command you want to execute in both branches!'
+    }
+  ])
+
+  const cmd = cmdPrompt.cmd
+
+  choices.forEach(async branch => {
+    console.log(Chalk.hex(RandomColor())(`Checking out "${branch}"`))
+    await git.checkout(branch)
+
+    console.log(Chalk.grey(`Execute "${cmd}"`))
+    SpawnSync(cmd, {
+      stdio: 'inherit',
+      shell: true,
+      encoding: 'utf-8'
     })
   })
-  .catch(console.error)
+}
+
+init()
