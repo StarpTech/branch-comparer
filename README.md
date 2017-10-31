ba92679d243b9f1125a1a96a4882112f13faf214
Dustin Deus <deusdustin@gmail.com>
2017-10-31 23:59:43 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # branch-comparer
-Checkout multiple Git Repositorys and execute scripts to compare the results all at once
+Checkout multiple git Repositorys and execute scripts.
+Run benchmark scripts in different repositorys and print them in the console.
+
+## Installation
+```
+npm i -g branch-comparer
+branchcmp
+```
+## Usage
+
+```
+? What's your branches to compare? master, test
+? Please enter the command you want to execute in both branches! npm run benchmark
+Checking out "master"
+Checking out "test"
+Execute "npm run benchmark"
+v8.8.1
+Execute "npm run benchmark"
+v8.8.1
+```
+
+## Features
+
+- Select multiple branches from a list
+- Execute any command synchronously
---
+++ index.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const Inquirer = require('inquirer')
+const Chalk = require('chalk')
+const RandomColor = require('randomcolor')
+const SpawnSync = require('child_process').spawnSync
+const git = require('simple-git/promise')(process.cwd())
+
+git
+  .branch()
+  .then(result => {
+    const branches = result.all
+    let branch1 = ''
+    let branch2 = ''
+
+    return Inquirer.prompt([
+      {
+        type: 'checkbox',
+        name: 'choice',
+        message: "What's your branches to compare?",
+        choices: branches,
+        validate: input => {
+          if (input.length < 2) {
+            return 'You have to select two branches!'
+          }
+          return true
+        }
+      }
+    ]).then(result => {
+      const choices = result.choice
+
+      return Inquirer.prompt([
+        {
+          type: 'input',
+          name: 'cmd',
+          message:
+            'Please enter the command you want to execute in both branches!'
+        }
+      ]).then(result => {
+        const cmd = result.cmd
+
+        choices.forEach(branch => {
+          console.log(Chalk.hex(RandomColor())(`Checking out "${branch}"`))
+          git
+            .checkout(branch)
+            .then(() => {
+              console.log(Chalk.grey(`Execute "${cmd}"`))
+              SpawnSync(cmd, {
+                stdio: 'inherit',
+                shell: true,
+                encoding: 'utf-8'
+              })
+            })
+            .catch(console.error)
+        })
+      })
+    })
+  })
+  .catch(console.error)
